b070f9428bd78b4a0d99375011cd8ff04a386a9b
Eric Silveira Gomes <es9694649@gmail.com>
2023-08-13 22:29:54 -0300

    Desafio finalizado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -3,14 +3,17 @@ class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
 
         let valorTotal = 0
-        let pedido = ""
+        let pedido = []
         let quantidade = 0
         const arrayPedidos = []
 
         const pratos = []
 
-
-        let resultadoFinal
+        //Ajustando os valores de entrada
+        if (Array.isArray(metodoDePagamento)) {
+            itens = metodoDePagamento
+            metodoDePagamento = ""
+        }
 
         //---------------------------------------------- ITENS ----------------------------------------------
 
@@ -19,32 +22,66 @@ class CaixaDaLanchonete {
             return "Não há itens no carrinho de compra!";
         } else {
 
-            //VERIFICANDO OS EXTRAS E PRINCIPAIS
             for (let index = 0; index < itens.length; index++) {
                 arrayPedidos[index] = itens[index].split(',')[0]
             }
 
-                for (let index = 0; index < itens.length; index++) {
+            for (let index = 0; index < itens.length; index++) {
 
-                    //PEGANDO VALORES ANTES E DEPOIS DA VÍRGULA
-                    pedido = itens[index].split(',')[0]
-                    quantidade = itens[index].split(',')[1]
+                //PEGANDO VALORES ANTES E DEPOIS DA VÍRGULA
+                pedido[index] = itens[index].split(',')[0]
+                quantidade = itens[index].split(',')[1]
 
-                    if (pedido == null || pedido === "") {
-                        return "Item inválido!";
-                    }
-                    else if (quantidade < 1 || !quantidade) {
-                        return "Quantidade inválida!";
-                    }
 
-                    valorTotal = valorTotal + somarPedido(pedido, quantidade)
+                //VERIFICANDO SE ESTÁ SEM O PEDIDO E SÓ COM A QUANTIDADE
+                if (!isNaN(pedido[index])) {
+                    quantidade = parseFloat(pedido[index]);
+                    pedido = ""
                 }
 
+                //console.log("\nItens"+itens)
+                // console.log("\nmETODO"+metodoDePagamento)
+
+
+                if (!pedido || pedido === "" || !["cafe", "chantily", "suco", "sanduiche", "queijo", "salgado", "combo1", "combo2"].includes(pedido[index])) {
+                    return "Item inválido!";
+                }
+
+                else if (quantidade < 1) {
+                    return "Quantidade inválida!"
+                }
+
+                valorTotal = valorTotal + somarPedido(pedido[index], quantidade)
+            }
+
+            if (verificarPratos(pedido)) {
                 if (valorTotal > 0) {
                     return pagamento(metodoDePagamento, valorTotal)
                 }
+            } else {
+                return "Item extra não pode ser pedido sem o principal"
+            }
         }
     }
+}
+
+function verificarPratos(itens) {
+
+    let verificacao = true
+
+    if (itens.includes('chantily') == true && itens.includes('cafe') == false) {
+        verificacao = false
+    }
+
+    if (itens.includes('suco') == true && itens.includes('salgado') == false) {
+        verificacao = false
+    }
+
+    if (itens.includes('queijo') == true && itens.includes('sanduiche') == false) {
+        verificacao = false
+    }
+
+    return verificacao
 }
 
 function somarPedido(pedido, quantidade) {
@@ -92,9 +129,9 @@ function pagamento(metodoDePagamento, valor) {
     }
 }
 
-// const resultado = new CaixaDaLanchonete();
+const resultado = new CaixaDaLanchonete();
 
-// console.log(
-//     resultado.calcularValorDaCompra('debito', ['cafe,1', 'chantily,1']));
+console.log(
+    resultado.calcularValorDaCompra(['pizza, 1']));
 
 export { CaixaDaLanchonete };
